c8be7ee15373181599a663864f87058c14350248
morival <64006835+morival@users.noreply.github.com>
2021-10-01 01:41:56 +0100

    get by id fixed

--- helpers/create-router.js
+++ helpers/create-router.js
@@ -6,25 +6,38 @@ const createRouter = function(collection) {
 
   const router = express.Router();
 
+//   GET ALL
   router.get('/', async (req, res) => {
+    const data = await collection.find().toArray()
     try {
-        const data = await collection.find().toArray()
         res.json(data)
-        // console.log(await collection.find().count())
+    } catch(err) {
+        res.status(500).json({ message: err.message})
+    }
+  });
+
+//   GET ONE
+  router.get('/:id', async (req, res) => {
+    //   res.send(res.user.name)
+    const data = await collection.find().toArray()
+    const id = req.params.id;
+    try {
+        if (await data.findOne({ _id: ObjectID(id)}))
+        res.send(data)
     } catch(err) {
         res.status(500).json({ message: err.message})
     }
     // collection
-    // .find()
-    // .toArray()
-    // .then((docs) => res.json(docs))
+    // .findOne({ _id: ObjectID(id) })
+    // .then((doc) => res.json(doc))
     // .catch((err) => {
     //   console.error(err);
     //   res.status(500);
     //   res.json({ status: 500, error: err });
     // });
-  });
+  })
 
+//   CREATE ONE
   router.post('/', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -41,7 +54,7 @@ const createRouter = function(collection) {
     }
   });
 
-  
+//   AUTHENTIFICATE
   router.post('/login', async (req, res) => {
     const data = await collection.find().toArray()
     const user = data.find(user => user.name === req.body.name)
@@ -57,13 +70,19 @@ const createRouter = function(collection) {
     }
   });
 
-
+//   UPDATE ONE
   router.put('/:id', async (req, res) => {
-    const data = await collection.find().toArray()
-    const id = req.params.id;
-    const updatedData = req.body;
-    delete updatedData._id;
-    data.findOneAndUpdate({ _id: ObjectID(id) }, { $set: updatedData })
+    try {
+        const data = await collection.find().toArray()
+        const id = req.params.id;
+        const updatedData = req.body;
+        console.log(updatedData)
+        delete updatedData._id;
+        data.findOneAndUpdate({ _id: ObjectID(id) }, { $set: updatedData })
+        res.json(data.value)
+    } catch {
+        res.status(500).send()
+    }
     // collection
     // .findOneAndUpdate({ _id: ObjectID(id) }, { $set: updatedData })
     // .then(result => {
@@ -75,7 +94,7 @@ const createRouter = function(collection) {
     // });
   });
 
-
+//   DELETE ONE
   router.delete('/:id', (req, res) => {
     const id = req.params.id;
     collection
@@ -92,5 +111,19 @@ const createRouter = function(collection) {
 
   return router;
 };
+
+async function getUser(req, res, next) {
+    let user
+    try {
+        user = await collection.findById(req.params.id)
+        console.log(user)
+        if (user == null)
+        res.status(404).json({message: "Cannot find user"})
+    } catch(err) {
+        res.status(500).send()
+    }
+    res.user = user
+    next()
+}
 
 module.exports = createRouter;
